e2aaf4e887d3fb1181223673be5d337d472c1e96
Prajwal V <89202178+10prajwal01@users.noreply.github.com>
2025-09-11 17:40:35 +0530

    environment variable implementation

--- datapipeline.py
+++ datapipeline.py
@@ -9,11 +9,14 @@ from langchain.vectorstores.cassandra import Cassandra
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain_core.runnables import RunnableLambda
 from urls import urls
-import cassio
+import cassio, os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-ASTRA_DB_APPLICATION_TOKEN = ""
-ASTRA_DB_ID = ""
+os.environ["ASTRA_DB_APPLICATION_TOKEN"] = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+os.environ["ASTRA_DB_ID"] = os.getenv("ASTRA_DB_ID")
+
 cassio.init(token=ASTRA_DB_APPLICATION_TOKEN, database_id=ASTRA_DB_ID)
 
 ## Vector store DB
